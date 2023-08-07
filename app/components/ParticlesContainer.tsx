b88decd5ac7441b04a6bc30017270fcf49b34481
ramazan97 <52708750+ramazan97@users.noreply.github.com>
2023-08-07 21:04:59 +0300

    Update ParticlesContainer.tsx

--- app/components/ParticlesContainer.tsx
+++ app/components/ParticlesContainer.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 import { useCallback } from "react";
 import { Particles } from "react-tsparticles";
 import { loadFull } from "tsparticles";
 import { Engine } from "tsparticles-engine";
 
-const ParticlesContainer: React.FC  = () => {
-  const particlesInit = useCallback(async (engine:Engine) => {
+const ParticlesContainer: React.FC = () => {
+  const particlesInit = useCallback(async (engine: Engine) => {
     await loadFull(engine);
   }, []);
 
@@ -13,22 +13,15 @@ const ParticlesContainer: React.FC  = () => {
 
   return (
     <Particles
-      className="w-full h-full absolute translate-z-0 "
       id="tsparticles"
       init={particlesInit}
       loaded={particlesLoaded}
       options={{
-        fullScreen: { enable: false },
-        background: {
-          color: {
-            value: "",
-          },
-        },
         fpsLimit: 120,
         interactivity: {
           events: {
             onClick: {
-              enable: false,
+              enable: true,
               mode: "push",
             },
             onHover: {
@@ -39,7 +32,7 @@ const ParticlesContainer: React.FC  = () => {
           },
           modes: {
             push: {
-              quantity: 90,
+              quantity: 4,
             },
             repulse: {
               distance: 200,
@@ -48,9 +41,11 @@ const ParticlesContainer: React.FC  = () => {
           },
         },
         particles: {
-          color: { value: "#e68e2e" },
+          color: {
+            value: "#ffffff",
+          },
           links: {
-            color: "#f5d393",
+            color: "#ffffff",
             distance: 150,
             enable: true,
             opacity: 0.5,
@@ -88,9 +83,87 @@ const ParticlesContainer: React.FC  = () => {
         },
         detectRetina: true,
       }}
-    >
-      ParticlesContainer
-    </Particles>
+    />
+
+    // <Particles
+    //   className="w-full h-full absolute translate-z-0 "
+    //   id="tsparticles"
+    //   init={particlesInit}
+    //   loaded={particlesLoaded}
+    //   options={{
+    //     fullScreen: { enable: false },
+    //     background: {
+    //       color: {
+    //         value: "",
+    //       },
+    //     },
+    //     fpsLimit: 120,
+    //     interactivity: {
+    //       events: {
+    //         onClick: {
+    //           enable: false,
+    //           mode: "push",
+    //         },
+    //         onHover: {
+    //           enable: true,
+    //           mode: "repulse",
+    //         },
+    //         resize: true,
+    //       },
+    //       modes: {
+    //         push: {
+    //           quantity: 90,
+    //         },
+    //         repulse: {
+    //           distance: 200,
+    //           duration: 0.4,
+    //         },
+    //       },
+    //     },
+    //     particles: {
+    //       color: { value: "#e68e2e" },
+    //       links: {
+    //         color: "#f5d393",
+    //         distance: 150,
+    //         enable: true,
+    //         opacity: 0.5,
+    //         width: 1,
+    //       },
+    //       collisions: {
+    //         enable: true,
+    //       },
+    //       move: {
+    //         direction: "none",
+    //         enable: true,
+    //         outModes: {
+    //           default: "bounce",
+    //         },
+    //         random: false,
+    //         speed: 1,
+    //         straight: false,
+    //       },
+    //       number: {
+    //         density: {
+    //           enable: true,
+    //           area: 800,
+    //         },
+    //         value: 80,
+    //       },
+    //       opacity: {
+    //         value: 0.5,
+    //       },
+    //       shape: {
+    //         type: "circle",
+    //       },
+    //       size: {
+    //         value: { min: 1, max: 5 },
+    //       },
+    //     },
+    //     detectRetina: true,
+    //   }}
+    // >
+    //   ParticlesContainer
+    // </Particles>
   );
 };
 

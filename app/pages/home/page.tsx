1d4900e830ada6189f45e65d8fc3f0de1429a26d
ramazan97 <52708750+ramazan97@users.noreply.github.com>
2023-08-09 17:13:19 +0300

    Update page.tsx

--- app/pages/home/page.tsx
+++ app/pages/home/page.tsx
@@ -12,13 +12,13 @@ const Page = () => {
       <div className="gradient-02" />
       <div className="w-full h-full  bg-gradient-to-r from-bg-gray-800 via-black">
         <div className="text-center flex flex-col  justify-center xl:pt-40 xl:text-left h-full container mx-auto">
-          <div className=" sm:pt-32">
+          <div className="  md:pt-16">
             <motion.h1
               variants={fadeIn("down", 0.2)}
               initial="hidden"
               animate="show"
               exit="hidden"
-              className="h1  font-fugaz  tracking-wider"
+              className="h1   font-fugaz  tracking-wider"
             >
               Hi, my name is <br />
               Ramazan I'm {"  "}
@@ -30,7 +30,7 @@ const Page = () => {
               initial="hidden"
               animate="show"
               exit="hidden"
-              className="max-w-sm font-fasthand xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+              className="max-w-sm font-fasthand xl:max-w-xl  mx-auto xl:mx-0 mb-10 xl:mb-16"
             >
               Hello! I am Ramazan. My own story with design and technology I'm
               telling. Electrical and Electronics during my university education
@@ -40,7 +40,7 @@ const Page = () => {
               In this site I share my work and projects. from contacting don't
               hesitate. Together we can sign great projects! Thanks!
             </motion.p>
-            <div className="flex relative z-50  justify-center xl:hidden ">
+            <div className="flex relative z-50   justify-center xl:hidden ">
               <ProjectsBtn />
             </div>
             <motion.div
